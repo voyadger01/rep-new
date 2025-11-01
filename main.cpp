c169e4685f45cbd8e3afac2bdb57f84467e5e58c
voyadger01 <voyadgerkx@gmail.com>
2025-11-01 12:29:48 +0300

    temp commit

--- main.cpp
+++ main.cpp
@@ -1,3 +1,22 @@
+#include <iostream>
+
+void output(const int** mtx);
+
+void rm(int** mtx);
+
+int** make(int rows, int cols) {
+
+}
+
 int main() {
-	
+  int rows = 0;
+  int cols = 0;
+  std::cin >> rows >> cols;
+  if (std::cin.fail()) {
+    return 1;
+  }
+  int** mtx = nullptr;
+  mtx = make(rows, cols);
+  output(mtx);
+  rm(mtx);
 }
