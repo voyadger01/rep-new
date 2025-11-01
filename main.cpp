a855e0964aa8305346ea344488b9585521930cf6
voyadger01 <voyadgerkx@gmail.com>
2025-11-01 17:46:20 +0300

    prepare commit

--- main.cpp
+++ main.cpp
@@ -4,6 +4,29 @@ void output(const int* const* mtx, int r, int c);
 void rm(int** mtx, int r);
 int** make(int r, int c);
 void input (int** mtx, int r, int c);
+int** convert (const int* t, size_t n, const size_t* lns, size_t rows);
+
+int main() {
+  int rows = 0;
+  int cols = 0;
+  std::cin >> rows >> cols;
+  if (std::cin.fail()) {
+    return 1;
+  }
+  int** mtx = nullptr;
+  try {
+    mtx = make(rows, cols);
+  } catch (const std::bad_alloc &) {
+    rm(mtx, rows);
+    return 2;
+  }
+  input(mtx, rows, cols);
+  if (std::cin.fail()) {
+    return 1;
+  }
+  output(mtx, rows, cols);
+  rm(mtx, rows);
+}
 
 void rm (int** mtx, int r) {
   for (size_t i = 0; i < r; ++i){
@@ -40,23 +63,4 @@ void output (const int* const* mtx, int r, int c) {
     }
     std::cout << '\n';
   }
-}
-
-int main() {
-  int rows = 0;
-  int cols = 0;
-  std::cin >> rows >> cols;
-  int** mtx = nullptr;
-  try {
-    mtx = make(rows, cols);
-  } catch (const std::bad_alloc &) {
-    rm(mtx, rows);
-    return 2;
-  }
-  input(mtx, rows, cols);
-  if (std::cin.fail()) {
-    return 1;
-  }
-  output(mtx, rows, cols);
-  rm(mtx, rows);
 }
